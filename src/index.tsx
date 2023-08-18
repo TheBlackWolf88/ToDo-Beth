949c1b35cca5065e237c2b24dde5e703d5b74ca6
TheBlackWolf88 <mopszi88@gmail.com>
2023-08-18 07:50:19 +0200

    yo this is crazy

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 
+
 const port = parseInt(process.env.PORT) || 3000;
 
 const app = new Hono();
@@ -7,16 +8,20 @@ const app = new Hono();
 let todos: string[] = [];
 
 const Layout = () => {
-    return (<html>
-        <head>
-            <script src="https://unpkg.com/htmx.org@1.9.4" integrity="sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV" crossorigin="anonymous"></script>
-            <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
-        </head>
-        <body>
-            <TodoList todos={todos} />
-            <TodoForm />
-        </body>
-    </html>)
+    return (
+        <html lang="en">
+            <head>
+                <script src="https://unpkg.com/htmx.org@1.9.4" integrity="sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV" crossorigin="anonymous"></script>
+                <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
+                <script src="https://cdn.tailwindcss.com"></script>
+            </head>
+            <body>
+                <div class="h-screen w-screen flex justify-center align-middle bg-zinc-700">
+                <TodoList todos={todos} />
+                <TodoForm />
+                </div>
+            </body>
+        </html>)
 }
 
 const TodoList = (todos: { todos: string[] }) => {
@@ -33,9 +38,11 @@ const TodoList = (todos: { todos: string[] }) => {
 
 const TodoForm = () => {
     return (
-        <form hx-post="/addTodo" hx-swap="beforeend" hx-target="#todoList" _="on htmx:afterRequest reset() me">
-            <input placeholder="More like Todone" name="todo" type="text" />
-            <button>Add Todo</button>
+        <form hx-post="/addTodo" hx-swap="beforeend" hx-target="#todoList" _="on htmx:afterRequest reset() me" class="h-fit mt-12">
+        <ul>
+            <li><input placeholder="More like Todone" name="todo" type="text" class="bg-zinc-500 text-white rounded-md p-4" /></li>
+            <li class="flex justify-center mt-4"><button class="bg-green-400 text-white border-2 border-green-600 p-4">Add Todo</button></li>
+        </ul>
         </form>
     )
 }
@@ -46,16 +53,15 @@ app.get("/", (c) => {
 
 });
 
-app.get("/try", (c) => {
-    return c.html(<h1>nt</h1>)
-});
-
 app.post("/addTodo", async (c) => {
     const todoBody = await c.req.parseBody()
     const todo = todoBody.todo
+    if (todo == "") {
+        return
+    }
     todos.push(todo)
     console.log(todos)
-    return c.html(<li _="on click remove me">{todo}</li>)
+    return c.html(<li _="on click remove me" class="text-green-500">{todo}</li>)
 });
 
 console.log(`Running at http://localhost:${port}`);
