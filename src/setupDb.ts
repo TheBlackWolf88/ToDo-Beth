25e5e313a24734e8e014ffa7315d21c30b1b4750
TheBlackWolf88 <mopszi88@gmail.com>
2023-08-23 05:22:22 +0200

    yo mr white

--- src/setupDb.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@libsql/client";
-import 'dotenv/config'
-
-const client = createClient({
-    url: process.env.DB_URL,
-    authToken: process.env.DB_TOKEN
-})
-
-let bs = await client.execute("create table todos (id text primary key, name text, isComplete integer);")
